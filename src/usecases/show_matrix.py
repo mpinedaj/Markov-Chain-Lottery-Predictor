8a9411f9eb0d60e188734c7aa9bbc954ecd04f10
David Navarro <ju.da.navarro@unillanos.edu.co>
2026-04-04 17:12:34 -0500

    rm trash

--- src/usecases/show_matrix.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def show_matrix(matriz_transicion):
-
-    for nombre, matriz in matriz_transicion.items():
-        print(f"{nombre}:")
-        print(pd.DataFrame(matriz)) 
-        print("\n")
